e47dedf2ca31ddf864495466b6cdcbccda46be5f
Grzegorz Bębenista <g.bebenista@interia.pl>
2021-05-07 12:56:22 +0200

    add efcore.sqlserver, add dbcontext

---
+++ MyCookingRecipes/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCookingRecipes
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Przepisy> Przepisy { get; set; }
+        public DbSet<Ulubione> Ulubione { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test");
+        }
+    }
+}
