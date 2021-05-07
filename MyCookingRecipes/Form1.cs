f20d1e0c81f033957ffddf75febde1a0bbda83ab
Grzegorz Bębenista <g.bebenista@interia.pl>
2021-05-07 12:06:14 +0200

    test commit

--- MyCookingRecipes/Form1.cs
+++ MyCookingRecipes/Form1.cs
@@ -15,6 +15,12 @@ namespace MyCookingRecipes
         public Form1()
         {
             InitializeComponent();
+
+            
+        }
+        private void Nothing()
+        {
+
         }
     }
 }
